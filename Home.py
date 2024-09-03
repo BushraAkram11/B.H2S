3690330c88a961795a89ead91d3e57dbf38efaed
BushraAkram11 <eagleit111@gmail.com>
2024-09-03 13:17:20 +0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -67,7 +67,7 @@ def extract_text_from_pdf(pdf_url: str) -> str:
     return text
 
 # Extract text from the provided PDF URL
-pdf_url = "https://drive.google.com/uc?export=download&id=1YN_vxYFcLGeTQ6KU4LCDmb-A5IMQPtGq"
+pdf_url = "https://drive.google.com/uc?export=download&id=12GSfTxJMpqtGKi5GWFZfIhBWRJ38nnVm"
 pdf_text = extract_text_from_pdf(pdf_url)
 
 # Ensure the user has provided an API key
