eafd9afd45ba87bd35693e761cafef31ce33669b
BushraAkram11 <eagleit111@gmail.com>
2024-09-03 11:12:43 +0500

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,90 @@
+from langchain_community.chat_message_histories import StreamlitChatMessageHistory
+import streamlit as st
+from langchain.prompts import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    MessagesPlaceholder,
+)
+from more_itertools import chunked
+
+# --- Logo ---
+st.set_page_config(page_title="Hope_To_Skill AI Chatbot", page_icon=":robot_face:")
+st.markdown(
+    """
+    <style>
+    .title {
+        text-align: center;
+        font-size: 36px;
+        font-weight: bold;
+        margin-bottom: 20px;
+    }
+    .subtitle {
+        text-align: center;
+        font-size: 24px;
+        color: gray;
+        margin-bottom: 20px;
+    }
+    .stTextInput input {
+        border: 2px solid black !important;
+        border-radius: 5px;
+        padding: 10px;
+        box-sizing: border-box;
+    }
+    .stTextInput {
+        margin-bottom: 20px;
+    }
+    </style>
+    <div class="title">Hope To Skill AI-Chatbot</div>
+    <div class="subtitle">Welcome to Hope To Skill AI Chatbot, How can I help you today?</div>
+    """,
+    unsafe_allow_html=True
+)
+
+# Sidebar with logo and Google API Key input
+with st.sidebar:
+    st.image("https://yt3.googleusercontent.com/G5iAGza6uApx12jz1CBkuuysjvrbonY1QBM128IbDS6bIH_9FvzniqB_b5XdtwPerQRN9uk1=s900-c-k-c0x00ffffff-no-rj", width=290)
+
+prompt = ChatPromptTemplate(
+    messages=[
+        MessagesPlaceholder(variable_name="chat_history"),
+        HumanMessagePromptTemplate.from_template("{question}"),
+    ]
+)
+
+msgs = StreamlitChatMessageHistory(key="langchain_messages")
+
+if len(msgs.messages) == 0:
+    msgs.add_ai_message("How can I assist you today?")
+
+for msg in msgs.messages:
+    st.chat_message(msg.type).write(msg.content)
+
+if prompt := st.chat_input():
+    st.chat_message("human").write(prompt)
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+
+        try:
+            _chat_history = st.session_state.langchain_messages[1:40]
+            _chat_history_tranform = list(
+                chunked([msg.content for msg in _chat_history], n=2)
+            )
+
+            from chain import chain
+            # Pass the google_api_key from session state here if needed
+            response = chain.stream(
+                {"question": prompt, "chat_history": _chat_history_tranform}
+            )
+
+            for res in response:
+                full_response += res or ""
+                message_placeholder.markdown(full_response + "|")
+                message_placeholder.markdown(full_response)
+
+            msgs.add_user_message(prompt)
+            msgs.add_ai_message(full_response)
+
+        except Exception as e:
+            st.error(f"An error occurred. {e}")
---
+++ chain.py
@@ -0,0 +1,119 @@
+from typing import List, Tuple
+import streamlit as st
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import (
+    ChatPromptTemplate,
+    MessagesPlaceholder,
+)
+from langchain_core.pydantic_v1 import BaseModel, Field
+from langchain_core.prompts.prompt import PromptTemplate
+from langchain_core.load import dumps, loads
+from pinecone import Pinecone
+from langchain_core.runnables import (
+    RunnableBranch,
+    RunnableLambda,
+    RunnableParallel,
+    RunnablePassthrough,
+)
+from operator import itemgetter
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_pinecone import PineconeVectorStore
+
+embeddings = HuggingFaceEmbeddings()
+
+# pass your google api key here
+
+google_api_key = "AIzaSyBXtfPk_O4kqGqyMK8iCD0KE_hfOCYAjUs"
+
+pinecone_api_key = "ae70f7c9-557a-4a1d-b944-5ecc208513ad"
+pc = Pinecone(api_key=pinecone_api_key)
+
+index_name = "hopetoskill"
+index = pc.Index(index_name)
+
+retriever = PineconeVectorStore(
+    embedding=embeddings, index=index
+).as_retriever()
+
+
+# Condense a chat history and follow-up question into a standalone question
+_template = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question, in its original language.
+Chat History:
+{chat_history}
+Follow Up Input: {question}
+Standalone question:"""  # noqa: E501
+CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(_template)
+
+# RAG answer synthesis prompt
+
+
+template = """You are an assistant for question-answering tasks for the Hope To Skill AI training initiative.Use the following retrieved context to answer the user's question:
+Question: {question} 
+Context: {context} 
+Answer:"""
+
+ANSWER_PROMPT = ChatPromptTemplate.from_messages(
+    [
+        ("system", template),
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("user", "{question}"),
+    ]
+)
+
+
+def _combine_documents(docs):
+    return "\n\n".join(set(dumps(doc) for doc in docs))
+
+
+def _format_chat_history(chat_history: List[Tuple[str, str]]) -> List:
+    buffer = []
+    for human, ai in chat_history:
+        buffer.append(HumanMessage(content=human))
+        buffer.append(AIMessage(content=ai))
+    return buffer
+
+
+# User input
+class ChatHistory(BaseModel):
+    chat_history: List[Tuple[str, str]] = Field(..., extra={"widget": {"type": "chat"}})
+    question: str
+
+
+_search_query = RunnableBranch(
+    # If input includes chat_history, we condense it with the follow-up question
+    (
+        RunnableLambda(lambda x: bool(x.get("chat_history"))).with_config(
+            run_name="HasChatHistoryCheck"
+        ),  # Condense follow-up question and chat into a standalone_question
+        RunnablePassthrough.assign(
+            chat_history=lambda x: _format_chat_history(x["chat_history"])
+        )
+        | CONDENSE_QUESTION_PROMPT
+        | ChatGoogleGenerativeAI(model="gemini-1.5-pro",temperature=0,api_key=google_api_key)
+        | StrOutputParser(),
+    ),
+    # Else, we have no chat history, so just pass through the question
+
+    RunnableLambda(itemgetter("question")),
+)
+
+_inputs = RunnableParallel(
+    {
+        "question": lambda x: x["question"],
+        "chat_history": lambda x: _format_chat_history(x["chat_history"]),
+        "context": _search_query | retriever | _combine_documents,
+    }
+)
+chain = (
+    (_inputs | ANSWER_PROMPT | ChatGoogleGenerativeAI(model="gemini-1.5-pro",api_key=google_api_key,temperature=0.4) | StrOutputParser())
+    .with_types(input_type=ChatHistory)
+    .with_fallbacks(
+        [
+            RunnableLambda(
+                lambda prompt: "There is an error while generating your response please try Again."
+            )
+        ]
+    )
+)
