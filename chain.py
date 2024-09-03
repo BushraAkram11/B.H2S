90d71b1666d6e4a54143e7fe486f98e5a97ce630
BushraAkram11 <eagleit111@gmail.com>
2024-09-03 13:25:08 +0500

    Update chain.py

--- chain.py
+++ chain.py
@@ -1,20 +1,12 @@
 from typing import List, Tuple
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import (
-    ChatPromptTemplate,
-    MessagesPlaceholder,
-)
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.pydantic_v1 import BaseModel, Field
 from langchain_core.prompts.prompt import PromptTemplate
-from langchain_core.load import dumps, loads
+from langchain_core.load import dumps
 from pinecone import Pinecone
-from langchain_core.runnables import (
-    RunnableBranch,
-    RunnableLambda,
-    RunnableParallel,
-    RunnablePassthrough,
-)
+from langchain_core.runnables import RunnableBranch, RunnableLambda, RunnableParallel, RunnablePassthrough
 from operator import itemgetter
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.messages import HumanMessage, AIMessage
